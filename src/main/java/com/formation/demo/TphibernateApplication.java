0706353a7a5c9f96c97ecd793f7ab8eaa6108b95
a-farsi <abdelhak.farsi@gmail.com>
2019-10-07 15:32:55 +0200

    Initial commit

---
+++ src/main/java/com/formation/demo/TphibernateApplication.java
@@ -0,0 +1,13 @@
+package com.formation.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TphibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TphibernateApplication.class, args);
+    }
+
+}
